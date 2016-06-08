3879183e4575d99d8cb8735ce8b04db03d41306b
crete_o <olivier.crete@epitech.eu>
2016-06-08 23:18:57 +0200

    enemies shoot collisions

--- js/game.js
+++ js/game.js
@@ -37,7 +37,7 @@ function init(){
     var startWrap = new createjs.Container();
     var end = false;
     var move = [false, false, false, false];
-    document.onkeydown = handleKeyDown;
+    //document.onkeydown = handleKeyDown;
     // document.onkeyup = handleKeyUp;
     //var shoot;
     var shootArray = [];
@@ -230,6 +230,20 @@ function init(){
                 }
             }
         }
+        //enemies shoot hit ship
+        for(var i=0; i<enemiesShootArr.length; i++){
+            if(ndgmr.checkPixelCollision(enemiesShootArr[i], ship, 0)){
+                console.log('you got hit !!');
+                stage.removeChild(enemiesShootArr[i]);
+                enemiesShootArr.splice(i, 1);
+                stage.removeChild(livesArray[livesArray.length-1]);
+                livesArray.splice(livesArray.length - 1, 1);
+                stage.update();
+                if( livesArray.length == 0){
+                    gameOver(0);
+                }
+            }
+        }
         //check if all enemies died and if so launch boss
         if(ennemiesArray.length == 0 && !bossPhase && !end){
             bossPhase = true;
@@ -414,71 +428,71 @@ function init(){
         }
     }
 
-    // $(document).keydown(function(e){
-    //     var key = e.keyCode;
-    //     if(key == 38 && ship.y > ship.image.height / 2){
-    //         createjs.Tween.get(ship)
-    //         .to({y: ship.y - 40}, 100, createjs.Ease.getPowInOut(1))
-    //         createjs.Ticker.setFPS(60);
-    //         // createjs.Ticker.addEventListener("tick", stage);
-    //         console.log('up');
-    //     }
-    //     else if(key == 39 && ship.x < stage.canvas.width - ship.image.width - 25){
-    //         createjs.Tween.get(ship)
-    //         .to({x: ship.x + 40}, 100, createjs.Ease.getPowInOut(1))
-    //         createjs.Ticker.setFPS(60);
-    //         // createjs.Ticker.addEventListener("tick", stage);
-    //         console.log('right');
-    //     }
-    //     else if(key == 40 && ship.y < stage.canvas.height - ship.image.height - 30){
-    //         createjs.Tween.get(ship)
-    //         .to({y: ship.y + 40}, 100, createjs.Ease.getPowInOut(1))
-    //         createjs.Ticker.setFPS(60);
-    //         // createjs.Ticker.addEventListener("tick", stage);
-    //         console.log('down ' + ship.y);
-    //     }
-    //     else if(key == 37 && ship.x > 0){
-    //         createjs.Tween.get(ship)
-    //         .to({x: ship.x - 40}, 100, createjs.Ease.getPowInOut(1))
-    //         createjs.Ticker.setFPS(60);
-    //         // createjs.Ticker.addEventListener("tick", stage);
-    //         console.log('left');
-    //     }
-    //     else if(key == 32 && canShoot && !paused){
-    //         var shoot = new createjs.Bitmap('img/' +img.fire[fireLevel]);
-    //         shoot.x = ship.x + (ship.image.width / 2) - (shoot.image.width / 2);
-    //         shoot.y = ship.y - (ship.image.height / 2);
-    //         shootArray.push(shoot);
-    //         stage.addChild(shoot);
-    //         var speed = (ship.y + 1000) * (4/3); 
-    //         createjs.Tween.get(shoot)
-    //         .to({y: - 1000}, speed, createjs.Ease.getPowInOut(1))
-    //         createjs.Ticker.setFPS(60);
-    //         // createjs.Ticker.addEventListener("tick", stage);
-    //         console.log('fire');
-    //     }
-    //     else if(key == 27){
-    //         if(!paused){
-    //             $('#canvas').css('animation-iteration-count', '0');
-    //             createjs.Ticker.setPaused(paused = true);
-    //             var pauseText = new createjs.Text('PAUSE', '75px RAVIE', 'white');
-    //             pauseText.x = 340;
-    //             pauseText.y = 300;
-    //             var restart = new createjs.Text('RESTART', '40px RAVIE', 'white');
-    //             restart.x = 380;
-    //             restart.y = 400;
-    //             $('#canvas').click(function(){location.reload();});;
-    //             pauseWrap.addChild(pauseText, restart);
-    //             stage.addChild(pauseWrap);
-    //             stage.alpha = 0.5;
-    //         }
-    //         else{
-    //             $('#canvas').css('animation-iteration-count', 'infinite');
-    //             createjs.Ticker.setPaused(paused = false);
-    //             stage.removeChild(pauseWrap);
-    //             stage.alpha = 1;
-    //         }
-    //     }
-    // });
+    $(document).keydown(function(e){
+        var key = e.keyCode;
+        if(key == 38 && ship.y > ship.image.height / 2){
+            createjs.Tween.get(ship)
+            .to({y: ship.y - 40}, 100, createjs.Ease.getPowInOut(1))
+            createjs.Ticker.setFPS(60);
+            // createjs.Ticker.addEventListener("tick", stage);
+            console.log('up');
+        }
+        else if(key == 39 && ship.x < stage.canvas.width - ship.image.width - 25){
+            createjs.Tween.get(ship)
+            .to({x: ship.x + 40}, 100, createjs.Ease.getPowInOut(1))
+            createjs.Ticker.setFPS(60);
+            // createjs.Ticker.addEventListener("tick", stage);
+            console.log('right');
+        }
+        else if(key == 40 && ship.y < stage.canvas.height - ship.image.height - 30){
+            createjs.Tween.get(ship)
+            .to({y: ship.y + 40}, 100, createjs.Ease.getPowInOut(1))
+            createjs.Ticker.setFPS(60);
+            // createjs.Ticker.addEventListener("tick", stage);
+            console.log('down ' + ship.y);
+        }
+        else if(key == 37 && ship.x > 0){
+            createjs.Tween.get(ship)
+            .to({x: ship.x - 40}, 100, createjs.Ease.getPowInOut(1))
+            createjs.Ticker.setFPS(60);
+            // createjs.Ticker.addEventListener("tick", stage);
+            console.log('left');
+        }
+        else if(key == 32 && canShoot && !paused){
+            var shoot = new createjs.Bitmap('img/' +img.fire[fireLevel]);
+            shoot.x = ship.x + (ship.image.width / 2) - (shoot.image.width / 2);
+            shoot.y = ship.y - (ship.image.height / 2);
+            shootArray.push(shoot);
+            stage.addChild(shoot);
+            var speed = (ship.y + 1000) * (4/3); 
+            createjs.Tween.get(shoot)
+            .to({y: - 1000}, speed, createjs.Ease.getPowInOut(1))
+            createjs.Ticker.setFPS(60);
+            // createjs.Ticker.addEventListener("tick", stage);
+            console.log('fire');
+        }
+        else if(key == 27){
+            if(!paused){
+                $('#canvas').css('animation-iteration-count', '0');
+                createjs.Ticker.setPaused(paused = true);
+                var pauseText = new createjs.Text('PAUSE', '75px RAVIE', 'white');
+                pauseText.x = 340;
+                pauseText.y = 300;
+                var restart = new createjs.Text('RESTART', '40px RAVIE', 'white');
+                restart.x = 380;
+                restart.y = 400;
+                $('#canvas').click(function(){location.reload();});;
+                pauseWrap.addChild(pauseText, restart);
+                stage.addChild(pauseWrap);
+                stage.alpha = 0.5;
+            }
+            else{
+                $('#canvas').css('animation-iteration-count', 'infinite');
+                createjs.Ticker.setPaused(paused = false);
+                stage.removeChild(pauseWrap);
+                stage.alpha = 1;
+            }
+        }
+    });
 
 }
