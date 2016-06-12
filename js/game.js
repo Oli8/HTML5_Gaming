ed6632354208e0a4588a1d1f612eae84e437f5a2
crete_o <olivier.crete@epitech.eu>
2016-06-12 19:02:07 +0200

    Level 5 easier

--- js/game.js
+++ js/game.js
@@ -17,7 +17,7 @@ function init(){
         {type: img.enemies[1], number: 3, shootY: 100, boss: img.bosses[1], pos: 200},
         {type: img.enemies[2], number: 4, shootY: 100, boss: img.bosses[2], pos: 175},
         {type: img.enemies[3], number: 5, shootY: 100, boss: img.bosses[3], pos: 150},
-        {type: img.enemies[4], number: 6, shootY: 100, boss: img.bosses[4], pos: 125}
+        {type: img.enemies[4], number: 5, shootY: 100, boss: img.bosses[4], pos: 150}
     ];
     var helpText = "The game consists of five phase, at the end of each\nyou will have to face the boss, you can not let it touch you\nor the game will end.\nUse the arrow key to move,\nthe spacebar to shoot\nand escape to pause.\nHave fun ! :)"; 
     //to do
